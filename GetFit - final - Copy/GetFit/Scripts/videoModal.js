048d2177ea8e75492aa679ed19916421d723dfc8
Bumchic <duonganhminh05@gmail.com>
2025-04-19 16:46:34 +0700

    get fit file

---
+++ GetFit - final - Copy/GetFit/Scripts/videoModal.js
@@ -0,0 +1,17 @@
+﻿@section Scripts {
+    <script>
+        $(document).ready(function() {
+            $('.video-btn').click(function () {
+                var videoUrl = $(this).data('video');
+                $('#videoSource').attr('src', videoUrl);
+                $('#workoutVideo')[0].load();
+                $('#videoModal').modal('show');
+            });
+
+        $('#videoModal').on('hidden.bs.modal', function() {
+            $('#workoutVideo')[0].pause();
+        $('#videoSource').attr('src', '');
+            });
+        });
+    </script>
+}
